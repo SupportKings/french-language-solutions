44c3f579720d12904e98cec468ebfd1ec415e9ca
vugvr617 <vnasraddinli@gmail.com>
2025-09-24 17:05:00 +0200

    update vercel config

--- apps/server/api/index.ts
+++ apps/server/api/index.ts
@@ -2,7 +2,7 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { app } from "../src/index";
 
 export const config = {
-	runtime: "nodejs20.x",
+	runtime: "nodejs",
 	maxDuration: 30,
 };
 
